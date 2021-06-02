ce1c0e2bce4a97051c64fe2d6f9a10afec71b509
decheine <decheine@hep.wisc.edu>
2021-06-01 19:04:27 -0500

    Runs and ends with a ROOT prompt

--- README.md
+++ README.md
@@ -18,4 +18,31 @@ You can run
 ```
 ./XDataFrame <args>
 ```
-To run the program
+To run the program.
+
+
+## Notes
+
+curl statement for making the web API call for ServiceX
+```bash
+curl -X POST http://localhost:5000/servicex/transformation   -H "Content-Type: application/json" -d submit_requst.json
+```
+Where `localhost:5000` is the address and port of your local server, or replaced by the user's endpoint. This is found in their `servicex.yaml` file, which is commonly and assumed to be located in the user's home directory. It looks something like
+
+```yaml
+api_endpoints:
+  - endpoint: https://xaod.servicex.ssl-hep.org/
+    token: somebigtokenthatistoomanycharactersandmathematicallyso.sinceanysecuritykeyinacryptographicsystema277characterkeyisdefinitelyoverkillthenagainicouldbewrongsincemylevelofknowledgeofcryptographyislimitedatbesttheonlyreasonthisstringexistsatallisbecauseifoundwritingthisveryamusing
+    type: xaod
+```
+
+
+
+
+
+
+
+## Features
+Expanding as I think of and syntesize these ideas
+- Read the `servicex.yaml` file from the user's home directory.
+
--- exe/XDataFrame.cxx
+++ exe/XDataFrame.cxx
@@ -3,6 +3,9 @@
 
 #include "../include/XDataFrameConfig.h.in"
 // Include ROOT
+#include "TF1.h"
+#include "TRint.h"
+#include "TCanvas.h"
 
 // Include ServiceX ?
 
@@ -11,17 +14,29 @@
 int main(int argc, char* argv[]){
     std::cout << "Hello world \n";
 
-    if (argc < 2) {
-        // report version
-        // Show usage 
-        std::cout << argv[0] << " Version " << XDataFrame_VERSION_MAJOR << "."
-                << XDataFrame_VERSION_MINOR << std::endl;
-        std::cout << "Usage: " << argv[0] << " number" << std::endl;
-        return 1;
-    }
+    // if (argc < 2) {
+    //     // report version
+    //     // Show usage 
+    //     std::cout << argv[0] << " Version " << XDataFrame_VERSION_MAJOR << "."
+    //             << XDataFrame_VERSION_MINOR << std::endl;
+    //     std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    //     return 1;
+    // }
+
+    // Runs basic ROOT app with the arguments given for main
+    TRint app("app", &argc, argv);
+
+    // Testing. Makes some drawings on a TCanvas
+    TCanvas* c = new TCanvas("c", "Something", 0, 0, 800, 600);
+    TF1 *f1 = new TF1("f1","sin(x)", -5, 5);
+    f1->SetLineColor(kBlue+1);
+    f1->SetTitle("My graph;x; sin(x)");
+    f1->Draw();
+    c->Modified(); c->Update();
+    //
 
 
-
-
+    // Run and finish with ROOT prompt
+    app.Run();
     return 0;
 }
