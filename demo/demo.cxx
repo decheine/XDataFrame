47b8e76c6ef6d6bea6d32e0abf8f5488eb4aac09
decheine <decheine@hep.wisc.edu>
2021-07-13 12:50:55 -0500

    adding demo executable

---
+++ demo/demo.cxx
@@ -0,0 +1,17 @@
+// Standard includes
+#include <stdlib.h>
+#include <iostream>
+
+
+// XDataFrame
+
+
+
+
+int main(int argc, char* argv[]){
+
+    std::cout << "Hello world, let's use RDataFrame\n"; 
+
+    return 0;
+
+}
