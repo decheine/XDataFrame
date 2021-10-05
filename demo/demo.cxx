437eec7fb8b57afb6b764dd2596484fa89b2c366
decheine <decheine@hep.wisc.edu>
2021-10-05 16:50:47 -0500

    add printed demo instructions

--- demo/demo.cxx
+++ demo/demo.cxx
@@ -15,12 +15,13 @@
 
 int main(int argc, char* argv[]){
 
-    std::string requestString = "{      \"did\": \"cernopendata://1507\",       \"selection\": \"(call ResultTTree (call Select (call SelectMany (call EventDataset 'ServiceXSourceCMSRun1AOD') (lambda (list e) (call (attr e 'TrackMuons') 'globalMuons'))) (lambda (list m) (call (attr m 'pt')))) (list 'mu_pt') 'treeme' 'file.root')\",     \"result-destination\": \"object-store\",     \"result-format\": \"root-file\",     \"chunk-size\": \"1000\",     \"workers\": \"10\"}";
+    std::string requestString = "{       \"did\": \"cernopendata://1507\",        \"selection\": \"(call ResultTTree (call Select (call SelectMany (call EventDataset 'ServiceXSourceCMSRun1AOD') (lambda (list e) (call (attr e 'TrackMuons') 'globalMuons'))) (lambda (list m) (call (attr m 'pt')))) (list 'mu_pt') 'treeme' 'file.root')\",     \"result-destination\": \"object-store\",     \"result-format\": \"root-file\",     \"chunk-size\": \"1000\",     \"workers\": \"10\"}";
 
     // Getting the RDataFrame
     ROOT::RDataFrame mydf = XDataFrame(requestString);
 
     std::cout << "Filtering\n";
+    std::cout << "Using filter mydf.Filter(\"mu_pt > 10\").Filter(\"mu_pt < 1000\").Histo1D(\"mu_pt\");"
 
     // Setting up canvas to draw on
     TCanvas* canvas = new TCanvas();
@@ -28,10 +29,19 @@ int main(int argc, char* argv[]){
     // Obtaining a 1D histogram after applying filters to mydf
     auto filtered_h = mydf.Filter("mu_pt > 10").Filter("mu_pt < 1000").Histo1D("mu_pt");
 
+
+    std::cout << "Printing\n";
     // Draw and save histogram to pdf
     filtered_h->Draw();
     canvas->Update();
     canvas->Print("demoFilter1.pdf");
+
+    std::cout << "A PDF of the output has been saved as \"demoFilter1.pdf\". \n";
+    std::cout << "If you want to copy that file from the container to your local machine, from a new terminal window, run command\n";
+    std::cout << "\n\t\t docker cp <container id>:/usr/src/xdataframe/demoFilter1.pdf ~\n\n";
+    std::cout << "You can get the container id from running \n\n ";
+    std::cout << "\t\t docker container ls  | grep 'decheine/xdataframe-demo-image' | awk '{print $1}' \n\n";
+    std::cout << "In a new terminal window. " 
 
     return 0;
 }
